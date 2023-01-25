323f901f97ea462b3e221b95dd8b5c90cfa3d744
warrensiro <warrensiro@gmail.com>
2023-01-25 09:14:38 +0000

    using modulus in loops

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-digit = number % 10
-else:
-new_num = number * -1
-digit = (new_num % 10) * -1
-if digit > 5:
-print('Last digit of', number, 'is', digit, 'and is greater than 5')
-elif digit == 0:
-print('Last digit of', number, 'is', digit, 'and is zero')
-else:
-print('Last digit of', number, 'is', digit, 'and is less than 6 and not 0')
+if ((number % 10) == 0):
+    print(f"Last digit of {number} is {number % 10} and is 0")
+while (number < 0):
+    if ((number % 10) > 4) and ((number % 10) != 0):
+        print(f"Last digit of {number} is {10 - (number % 10)} and is less than 6 and not 0")
+    elif ((number % 10) < 5) and ((number % 10) != 0):
+        print(f"Last digit of {number} is {10 - (number % 10)} and is greater than 5")
+    break
+while (number > 0):
+    if ((number % 10) > 5) and ((number % 10) != 0):
+        print(f"Last digit of {number} is {number % 10} and is greater than 5")
+    elif ((Number % 10) < 6) and ((number % 10) != 0):
+        print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+    break
