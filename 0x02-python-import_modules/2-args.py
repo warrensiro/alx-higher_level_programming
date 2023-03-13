f5f5f0a60f81461114ecbfc27d2afdeb3f65046a
warrensiro <warrensiro@gmail.com>
2023-03-13 10:26:28 +0000

    printing list of arguments and indexes of the arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print(len(argv), "argument:")
+        print((len(argv) - 1), "arguments.")
+    elif len(argv) == 2:
+        print((len(argv) - 1), "argument:")
         for i in argv:
-            print("{} : {}".format(argv.index(i) + 1, i))
-    elif len(argv) == 0:
-        print(len(argv), "arguments.")
-    elif len(argv) > 1:
-        print(len(argv), "arguments:")
+            print("{} : {}\n".format(argv.index(i) + 1, i))
+    elif len(argv) > 2:
+        print((len(argv) - 1), "arguments:")
         for i in argv:
-            print("{} : {}".format(argv.index(i) + 1, i))
+            print("{} : {}\n".format(argv.index(i) + 1, i))
