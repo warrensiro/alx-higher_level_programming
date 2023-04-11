750726ca787de564c002f36781813f1c3ea25a67
warrensiro <warrensiro@gmail.com>
2023-04-11 10:20:16 +0000

    replacing  an element in a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx > len(my_list) - 1):
-        continue
+        return my_list
     i = 0
     while True:
         if i == idx:
             my_list[i] = element
             return my_list
+        i += 1
