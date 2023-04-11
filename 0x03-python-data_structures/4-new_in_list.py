f3eb6aaceddf24a6f8e07d18fa4b0cab6e83c0e8
warrensiro <warrensiro@gmail.com>
2023-04-11 10:54:32 +0000

    replacing an item in a new list without altering the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (idx < 0) or (idx > (len(my_list) - 1)):
+        return my_list
+    new_list = []
+    i = 0
+    while True:
+        if i == idx:
+            new_list = my_list[i] = element
+            return new_list
+        i += 1
