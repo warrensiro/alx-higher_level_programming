bb1160d8602fd5b9fc32a4b12f403e8535a86ad7
warrensiro <warrensiro@gmail.com>
2023-04-13 07:30:37 +0000

    appending items to a new list while retaining original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,10 @@
 def new_in_list(my_list, idx, element):
     if (idx < 0) or (idx > (len(my_list) - 1)):
         return my_list
-    new_list = []
+    new_list = my_list.copy()
     i = 0
     while True:
         if i == idx:
-            new_list = my_list[i] = element
+            new_list[i] = element
             return new_list
         i += 1
