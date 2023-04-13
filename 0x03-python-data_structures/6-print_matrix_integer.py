ab2f269e36a609fcba0a2aed7baaebc74ccfb87c
warrensiro <warrensiro@gmail.com>
2023-04-13 09:32:05 +0000

    function to print a 2-d array

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for c in i:
-            print("{:d}".format(c) end=" ")
+            print("{:d}".format(c), end=" ")
         print()
