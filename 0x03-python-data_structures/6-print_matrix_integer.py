df34686283c1e1f358d211eecb6866a1874b23aa
warrensiro <warrensiro@gmail.com>
2023-04-13 10:06:33 +0000

    function to print a 2-d array

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix = [[]] or matrix is None:
+    if matrix == [[]] or matrix is None:
         print()
     for i in matrix:
         b = 1
