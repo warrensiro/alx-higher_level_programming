e2fa3e8910e08f7f1eeccf9901d0afb949a8f2bf
warrensiro <warrensiro@gmail.com>
2023-04-16 21:31:27 +0000

    function to check the maximum integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    temp = my_list[0]
+    for i in my_list:
+        if i > temp:
+            temp = i
+    return temp  
