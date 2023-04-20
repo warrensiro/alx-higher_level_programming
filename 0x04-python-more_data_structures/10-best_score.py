aa6bd4d5fddec42ce5ef1f99de6776c5bf57cf1c
warrensiro <warrensiro@gmail.com>
2023-04-20 22:45:57 +0000

    function to determine the key with biggest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary = {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     biggest_value = max(a_dictionary, key=a_dictionary.get)
     return biggest_value
