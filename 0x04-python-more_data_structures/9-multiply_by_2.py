4d0c5d1326fee3dc1898ff48964a284089a08f44
warrensiro <warrensiro@gmail.com>
2023-04-20 22:30:10 +0000

    function to multiply all values by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    dict_1 = list(a_dictionary.keys())
-    dict_1.sort()
-    for i in dict_1:
-        print("{:s}: {}".format(i, a_dictionary[i*2]))
+    new_dict = {}
+    for i in a_dictionary:
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict
