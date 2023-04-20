10ab13b288eb12cf1cb6a7b15d3a0379cacaa2ed
warrensiro <warrensiro@gmail.com>
2023-04-20 22:18:36 +0000

    function to delete a particular key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,4 +2,5 @@
 def simple_delete(a_dictionary, key=""):
     get_key = key in a_dictionary
     if get_key is True:
-        return a_dictionary.pop(key)
+        del a_dictionary[key]
+    return a_dictionary
