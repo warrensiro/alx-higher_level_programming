e8e1df43230387a11ac034b9d4c6baf4e684781e
warrensiro <warrensiro@gmail.com>
2023-04-20 11:02:44 +0000

    function to sort and list keys in a dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     dict_1 = list(a_dictionary.keys())
     dict_1.sort()
-    dict_1 = {i: a_dictionary[i] for i in dict_1}
-    return dict_1
+    for i in dict_1: 
+        print("{:s}: {}".format(i, a_dictionary[i]))
