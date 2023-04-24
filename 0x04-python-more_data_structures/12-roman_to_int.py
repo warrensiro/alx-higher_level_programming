9461698ceddff1d792cc327daa7b47f966e5ac7a
warrensiro <warrensiro@gmail.com>
2023-04-24 08:37:13 +0000

    function to determine roman numerals in numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     number = 0
-    if roman_string == "" or isInstance(roman_string, str) is False:
+    if roman_string == "" or isinstance(roman_string, str) is False:
         return 0
     for i in range(len(roman_string)):
         if roman_string[i] == "M":
