ce51be9454dc09372efa0631b1c859d22ff710c4
warrensiro <warrensiro@gmail.com>
2023-04-24 08:33:28 +0000

    function to determine roman numerals in numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,27 +7,27 @@ def roman_to_int(roman_string):
         if roman_string[i] == "M":
             number += 1000
             if roman_string[i-1] == "C" and i != 0:
-                number -= 100
+                number -= 200
         if roman_string[i] == "D":
             number += 500
             if roman_string[i-1] == "C" and i != 0:
-                number -= 100
+                number -= 200
         if roman_string[i] == "C":
             number += 100
             if roman_string[i-1] == "X" and i != 0:
-                number -= 10
+                number -= 20
         if roman_string[i] == "L":
             number += 50
             if roman_string[i-1] == "X" and i != 0:
-                number -= 10
+                number -= 20
         if roman_string[i] == "X":
             number += 10
             if roman_string[i-1] == "I" and i != 0:
-                number -= 1
+                number -= 2
         if roman_string[i] == "V":
             number += 5
             if roman_string[i-1] == "I" and i != 0:
-                number -= 1
+                number -= 2
         if roman_string[i] == "I":
             number += 1
     return number
