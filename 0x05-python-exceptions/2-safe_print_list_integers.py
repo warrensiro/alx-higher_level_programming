2fede89f5bff95fb95dc68dd7a0b03992ea1a542
warrensiro <warrensiro@gmail.com>
2023-04-26 09:56:44 +0000

    function to print only integers in range

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,8 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     for i in range(x):
         try:
-            if any(char.isdigit()):
-                print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
             print("")
             return x
         except (IndexError, TypeError) as e:
