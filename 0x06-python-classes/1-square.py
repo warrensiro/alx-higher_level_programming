7c8bf242079e5972f144d70242786c768d73f8b2
warrensiro <warrensiro@gmail.com>
2023-05-05 10:03:09 +0000

    passing a private instance attribute to the initialize method

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+class Square:
+    """initialization method"""
+    def __init__(self, size):
+        """private instance attribute of size"""
+        self.__size = size
