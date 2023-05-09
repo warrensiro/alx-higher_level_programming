3cb812760884c14feff504faafb39f3223d457b0
warrensiro <warrensiro@gmail.com>
2023-05-09 12:30:14 +0000

    check if argument is an integer

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,15 +3,10 @@
 class Square:
     """initialization method"""
     def __init__(self, size=0):
-        """checking type of size"""
-        """continue with execution if an integer else raise the exception"""
+        """validate size as int"""
         if not type(size) is int:
-            """Exception raised"""
             raise TypeError("size must be an integer")
-        """continue if size is positive"""
         elif size < 0:
-            """Exception raised"""
             raise ValueError("size must be >= 0")
-        """Assign the positive integer to a private instance attribute size"""
         else:
             self.__size = size
