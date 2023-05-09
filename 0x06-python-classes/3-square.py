f485bc0c995ee3efa7358962b10fb2abf983d468
warrensiro <warrensiro@gmail.com>
2023-05-09 13:11:02 +0000

    class to compute area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,6 +12,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """method to compute area"""
         return self.__size * self.__size
