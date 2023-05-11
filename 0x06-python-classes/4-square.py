981a787f6a57babf631577cdbec177f79ef41c9f
warrensiro <warrensiro@gmail.com>
2023-05-11 08:14:32 +0000

    decorators

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+
+class Square:
+    """initialization method"""
+    def __init__(self, size=0):
+        """validate size as int"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """method to compute area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """getter decorator"""
+        return '{}'.format(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter decorator"""
+        self.__size = value 
