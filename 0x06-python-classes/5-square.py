2536364449583bc7ff924df0e8f346461f151269
warrensiro <warrensiro@gmail.com>
2023-05-11 09:23:33 +0000

    print the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
+"""class that defines a square"""
 
+
+class Square:
+    """initialization method"""
+    def __init__(self, size=0):
+        """validate size as int"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """method to compute area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """getter decorator"""
+        return '{}'.format(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter decorator"""
+        self.__size = value
+
+    def my_print(self):
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
