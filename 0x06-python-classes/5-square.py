0a9936f7e1a2453b709ea8804544f36f3193e5fc
warrensiro <warrensiro@gmail.com>
2023-05-11 09:33:12 +0000

    print the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -29,9 +29,9 @@ class Square:
 
     def my_print(self):
         """print the square"""
-        if self.__size = 0:
+	if self.__size == 0:
             print()
-	for i in range(self.__size):
+        for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
             print()
